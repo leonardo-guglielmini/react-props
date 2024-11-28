372fa53c4860428028f42f2127a8e5e11eaf9f39
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-28 16:47:24 +0100

    ADD main.jsx | tag section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,7 @@
 import style from "./main.module.css"
 import Card from "./ui/Card/card"
 import {posts} from "../data/posts"
+const tags = []
 
 export default function Main(){
     return(
@@ -11,8 +12,15 @@ export default function Main(){
                         post.published===true ?
                         <div className={style.col3} key={post.id}>
                             <Card title={post.title} image={post.image} content={post.content} tags={post.tags} published={post.    published}/>
+                            {post.tags.forEach(tag => !tags.find((tagEl) => tagEl === tag) ? tags.push(tag) : null)}
                         </div> : null
                     )}
+                </div>
+                <div className={style.tagList}>
+                    <h3>Tag presenti</h3>
+                    <ul>
+                        {tags.map((tag, index) => <li className={`${style.tag} ${tag === "html" ? style.htmlTag : tag==="js" ? style.jsTag : tag === "css" ? style.cssTag : style.phpTag}`} key={index}>{tag}</li> )}
+                    </ul>
                 </div>
             </div>
         </main>
