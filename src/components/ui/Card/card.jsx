a17b62b2468557f2a163d3d96283cb5c4529e2b6
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-28 16:17:35 +0100

    FIXED card.jsx | card heigth, tags display inline-block

--- src/components/ui/Card/card.jsx
+++ src/components/ui/Card/card.jsx
@@ -4,12 +4,16 @@ import style from "./card.module.css"
 export default function Card({title ="", image, content="",tags="",published}){
     return(
         <div className={style.card}>
-            <img className={style.thumb} src={image || placeholderImg}/>
-            <div className={style.cardBody}>
-                <h3>{title}</h3>
-                <p>{content}</p>
-                {tags.map((tag,index) => <p key={index}>{tag}</p>)}
-                <button><a href="#">Leggi di più</a></button>
+            <div className={style.col12}>
+                <img className={style.thumb} src={image || placeholderImg}/>
+            </div>
+            <div className={style.col12}>
+                <div className={style.cardBody}>
+                    <h3>{title}</h3>
+                    <p>{content}</p>
+                    {tags.map((tag,index) => <p className={style.tag} key={index}>{tag}</p>)}
+                    <button><a href="#">Leggi di più</a></button>
+                </div>
             </div>
         </div>
     )
